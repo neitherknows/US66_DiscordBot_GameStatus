9157f9387a4538c081dbd53712d0d44f61835682
neitherknows <tmn.nikita@gmail.com>
2024-10-09 19:15:08 +0500

    Delete bot.bat

--- bot.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-set scriptname=DiscordKPLiberation
-set scriptpriority=LOW
-set scriptexe=node index.js
-set scriptpath=%~dp0
-
-start /WAIT "%scriptname%" /D "%scriptpath%" /%scriptpriority% /B CMD /c "%scriptexe%"
